d2cf154edf02b43168254dfa927621e36f29ac42
yandaniugithub <154817501@qq.com>
2022-05-09 10:57:34 +0800

    first

---
+++ consul+polly+oclet+identityserver4+cap/code/Yak.Cap.Demo/Yak.Cap.RabbitMQ.SubscribeApi/Controllers/ConsumerController.cs
@@ -0,0 +1,32 @@
+﻿using DotNetCore.CAP;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Yak.Cap.RabbitMQ.Models;
+
+namespace Yak.Cap.InMemory.Api.Controllers
+{
+    public class ConsumerController : Controller
+    {
+        /// <summary>
+        ///  接收下单库存验证消息
+        /// </summary>
+        /// <param name="Order"></param>
+        /// <param name="header"></param>
+        /// <returns></returns>
+        [NonAction]
+        [CapSubscribe("ProductCheckHouseNum")]
+        public Order ProductCheckHouseNum(Order Order, [FromCap] CapHeader header)
+        {
+
+            if (Order != null)
+            {
+                Console.WriteLine(Order.SkuName);
+            }
+            return Order;
+
+        }
+
+    }
+}
---
+++ consul+polly+oclet+identityserver4+cap/code/Yak.Ocelot.Demo/IdentityServer_ResourceOwnerPasswordCredentials/Config.cs
@@ -0,0 +1,87 @@
+﻿// Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+
+
+using IdentityServer4;
+using IdentityServer4.Models;
+using IdentityServer4.Test;
+using System.Collections.Generic;
+using System.Security.Claims;
+
+namespace IdentityServer_ResourceOwnerPasswordCredentials
+{
+    public static class Config
+    {
+        //public static IEnumerable<IdentityResource> IdentityResources =>
+        //    new IdentityResource[]
+        //    { 
+        //        new IdentityResources.OpenId()
+        //    };
+
+        public static IEnumerable<ApiScope> ApiScopes =>
+            new ApiScope[]
+            { 
+                new ApiScope("Yak.Ocelot.Api", "My API"),
+                new ApiScope("Yak.Cap.RabbitMQ.SubscribeApi", "Product API"),
+                new ApiScope("Yak.Cap.RabbitMQ.PublisherApi", "Order API"),
+                new ApiScope("Yak.Microservice.Product.Api", "Yak.Microservice.Product API")
+            };
+
+        //public static IEnumerable<Client> Clients =>
+        //    new Client[] 
+        //    { };
+
+
+        public static List<TestUser> GetUsers()
+        {
+            return new List<TestUser>
+            {
+                new TestUser
+                {
+                    SubjectId = "1",
+                    Username = "alice",
+                    Password = "password"
+                },
+                new TestUser
+                {
+                    SubjectId = "2",
+                    Username = "bob",
+                    Password = "password"
+                },
+                new TestUser
+                {
+                    SubjectId = "3",
+                    Username = "yak",
+                    Password = "yakpassword"
+                }
+            };
+        }
+
+        public static IEnumerable<IdentityResource> GetIdentityResources()
+        {
+            return new IdentityResource[]
+            {
+                new IdentityResources.OpenId(),//未添加导致scope错误
+                new IdentityResources.Profile()
+            };
+        }
+
+        public static IEnumerable<Client> GetClients()
+        {
+            return new List<Client>
+            {
+                new Client
+                {
+                    ClientId = "yakclient",
+                    AllowedGrantTypes = GrantTypes.ResourceOwnerPassword,
+
+                    ClientSecrets =
+                    {
+                        new Secret("yaksecret".Sha256())
+                    },
+                    AllowedScopes = new []{ "Yak.Microservice.Product.Api", "Yak.Ocelot.Api", "Yak.Cap.RabbitMQ.SubscribeApi", "Yak.Cap.RabbitMQ.PublisherApi", IdentityServerConstants.StandardScopes.OpenId, IdentityServerConstants.StandardScopes.Profile }//需要额外添加 
+                }
+            };
+        }
+    }
+}
